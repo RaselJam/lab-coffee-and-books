371588b7c7e6020895c1a45f3f5616f3a0e4f42a
RaselJam <Rasel.jam.h@gmail.com>
2021-11-17 17:57:45 +0100

    Not Done: tengo problemas en construir el proyecto completo se como fonciona pieza a pieza perono consigo conectar todocon fluidez

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,65 @@
+let _axios = axios.create({
+	baseURL: 'http://localhost:3000',
+})
+
 document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("lab-coffee-and-books JS imported successfully!");
-  },
-  false
-);
+	'DOMContentLoaded',
+	async () => {
+		console.log('lab-coffee-and-books JS imported successfully!')
+		console.log('laoding .....')
+		let loaded = await loadAll()
+		console.log('Loaded : ', loaded)
+		populateAll(loaded.data.data)
+		document.getElementById('submit-e').addEventListener('click', async function (e) {
+			//Updating :
+			e.preventDefault()
+			let name = document.getElementById('name-e').value
+			let storeType = document.getElementById('storeType-e').value
+			_axios
+				.put('/places', { id, name, storeType })
+				.then((r) => {
+					console.log('Updated')
+				})
+				.catch((err) => console.log(err))
+			let loaded = await loadAll()
+			populateAll(loaded.data.data)
+		})
+		document.getElementById('submit').addEventListener('click', async function (e) {
+			e.preventDefault()
+			let name = document.getElementById('name').value
+			let storeType = document.getElementById('storeType').value
+			let resultonPost = _axios
+				.post('/places', { name, storeType })
+				.then((r) => {
+					let container = document.getElementById('places-container')
+					populateRecord(r.data.added, container)
+				})
+				.catch((err) => console.log('Error: ', err))
+		})
+	},
+	false
+)
+
+function populateRecord(place, targetHTML) {
+	let { name, type, id } = place
+	console.log('in single pop: ', name, type)
+	let toPriint = `<div onClick=getEditForm.bind(${id},${name},${type}) id=${id} class="place"><div>NAME : ${name}</div><div> Type : ${type}</div></div>`
+
+	targetHTML.innerHTML += toPriint
+}
+async function loadAll() {
+	return await _axios.get('/places')
+}
+
+function populateAll(data) {
+	console.log('in pop', data[0])
+	let container = document.getElementById('places-container')
+	for (let i = 0; i < data.length; i++) {
+		console.log('data', data)
+		populateRecord(data[i], container)
+	}
+}
+function getEditForm(id, name, type) {
+	console.log('hey')
+	console.log(id, name, type)
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
-const router = require("express").Router();
+const router = require('express').Router()
+const Place = require('../models/place.model')
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
 
-module.exports = router;
+router.get('/', (req, res, next) => {
+	let toPrint
+
+	res.render('index', toPrint)
+})
+router.get('/places', (req, res) => {
+	let data = Place.find()
+		.then((r) => {
+			console.log('data :', r)
+			res.status(200).json({ data: r })
+		})
+		.catch((err) => console.log('Error: ', err))
+})
+router.put('/places', (req, res) => {
+	console.log('In server on update : ', req.body)
+})
+router.post('/places', (req, res) => {
+	let { name, storeType } = req.body
+	Place.create({ name, type: storeType })
+		.then((result) => {
+			res.status(200).json({ added: result })
+		})
+		.catch((err) => {
+			console.log(err)
+			res.status(500).jason({ error: err })
+		})
+})
+module.exports = router
